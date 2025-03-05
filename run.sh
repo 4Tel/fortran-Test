8fc9c612367debb2930f4ffde5fb6c154b861232
4tel <kyako95@gmail.com>
2025-03-05 15:36:36 +0900

    initial setting

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+src="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+if [ $# -eq 0 ];then
+  x=$(echo -e "\n1. run local \n2. run qfile\nchoice: ")
+  read -p "$x" i
+else
+  i="$@"
+fi
+
+case "$i" in
+  1) time mpirun -n 4 main.x;;
+  2) qsub "$src/qfile";;
+  *) echo "invalid value: $i";exit 1;;
+esac
