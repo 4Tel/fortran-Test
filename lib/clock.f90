2b31264273c045fd447e87a25685949cbc365e40
4tel <kyako95@gmail.com>
2025-05-30 18:28:57 +0900

    change name (make.inc > make.mk)

---
+++ lib/clock.f90
@@ -0,0 +1,19 @@
+MODULE clock
+  IMPLICIT NONE
+  !
+  REAL::start_cpu, end_cpu
+  INTEGER::start_cnt, end_cnt, cnt_rate, cnt_max
+CONTAINS
+  SUBROUTINE start_clock()
+    CALL CPU_TIME(start_cpu)
+    CALL SYSTEM_CLOCK(start_cnt, cnt_rate, cnt_max)
+  END SUBROUTINE start_clock
+  !
+  SUBROUTINE end_clock()
+    CALL CPU_TIME(end_cpu)
+    CALL SYSTEM_CLOCK(end_cnt, cnt_rate, cnt_max)
+    WRITE (*, *) 'CPU time   : ', end_cpu - start_cpu
+    WRITE (*, *) 'Wall time  : ', REAL(end_cnt - start_cnt)/REAL(cnt_rate)
+    WRITE (*, *)
+  END SUBROUTINE end_clock
+END MODULE clock
