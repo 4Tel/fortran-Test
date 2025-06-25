831edf13ca526b0bddccf95c6c77be33a790cb35
4Tel <kyako95@gmail.com>
2025-06-25 12:23:23 +0900

    cmplx / precison

--- base/complex/main.f90
+++ base/complex/main.f90
@@ -1,7 +1,7 @@
 PROGRAM main
   IMPLICIT NONE
-  COMPLEX(kind=8)::var
-  var = (1.0, 2.0)
+  COMPLEX(kind=4)::var
+  var = dcmplx(1.0, 2.0D+39)
   WRITE (*, *) "var = ", var
   WRITE (*, *) "real part = ", DBLE(var), REAL(var)
   WRITE (*, *) "imaginary part = ", AIMAG(var)
