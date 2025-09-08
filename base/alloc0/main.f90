00f1164401352a8c55be50e924c480fd73b7502e
4Tel <kyako95@gmail.com>
2025-09-08 10:21:27 +0900

    .

--- base/alloc0/main.f90
+++ base/alloc0/main.f90
@@ -3,7 +3,9 @@ PROGRAM main
   INTEGER, PARAMETER::stdin = 5
   INTEGER::tmp
   INTEGER, ALLOCATABLE::idx(:, :)
+  WRITE (*, '(A)', ADVANCE='NO') 'Enter an integer: '
   READ (stdin, *) tmp
   ALLOCATE (idx(3, tmp))
+  idx = 0
   WRITE (*, '(I5, I5)') tmp, SIZE(idx)
 END PROGRAM main
